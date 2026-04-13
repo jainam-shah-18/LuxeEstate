c8c12dd38d85b065a4e5e5821e440d1442f37027
Jainam-Shah-DS <124162798+jainam-shah-18@users.noreply.github.com>
2026-04-13 18:37:15 +0530

    Delete favorites directory

--- favorites/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Favorite
-
-
-@admin.register(Favorite)
-class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'property', 'created_at')
