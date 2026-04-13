c9ac870f3b3e55bf928ef6afa30eaa3830e30ea3
Jainam-Shah-DS <124162798+jainam-shah-18@users.noreply.github.com>
2026-04-13 18:37:46 +0530

    Delete properties directory

--- properties/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-
-def site_settings(request):
-    return {
-        'GOOGLE_MAPS_API_KEY': getattr(settings, 'GOOGLE_MAPS_API_KEY', ''),
-        'RAZORPAY_KEY_ID': getattr(settings, 'RAZORPAY_KEY_ID', ''),
-    }
