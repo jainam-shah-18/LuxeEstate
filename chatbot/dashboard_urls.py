cd8554f9dc64a0d0098a728b26c1420ca5e27eb6
Jainam-Shah-DS <124162798+jainam-shah-18@users.noreply.github.com>
2026-04-13 18:36:43 +0530

    Delete chatbot/dashboard_urls.py

--- chatbot/dashboard_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import dashboard_views
-
-app_name = 'chatbot_dashboard'
-
-urlpatterns = [
-    path('dashboard/', dashboard_views.ChatbotDashboardView.as_view(), name='dashboard'),
-]
